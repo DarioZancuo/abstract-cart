661033e74353707346c2a4d59a02890e09250fd8
candidato <candidato@MI-LAP-02.mshome.net>
2026-04-16 14:34:13 +0200

    Merge branch 'main' of https://github.com/DarioZancuo/demo-cart.git into main

--- backend/carrello-spring/src/main/java/com/ecommerce/cart/dto/ProductDTO.java
+++ backend/carrello-spring/src/main/java/com/ecommerce/cart/dto/ProductDTO.java
@@ -1,12 +1,14 @@
 package com.ecommerce.cart.dto;
 
+import java.math.BigDecimal;
+
 import com.ecommerce.cart.entity.jpa.Category;
 
 public record ProductDTO(
 	    Long id_prod, 
 	    String name, 
 	    String description,
-	    Double price,
+	    BigDecimal price,
 	    Long id_cat,
 	    String categoryName
 	    
