a9450949461c154a5bbec75363ab83522bdb36d6
candidato <candidato@MI-LAP-02.mshome.net>
2026-04-16 11:39:06 +0200

    aggiunta config openapi

---
+++ backend/carrello-spring/src/main/java/com/ecommerce/cart/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.ecommerce.cart;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+	@Bean
+	public OpenAPI customOpenAPI() {
+		final String securitySchemeName = "bearerAuth";
+		return new OpenAPI().addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+				.components(new Components().addSecuritySchemes(securitySchemeName,
+						new SecurityScheme().name(securitySchemeName).type(SecurityScheme.Type.HTTP).scheme("bearer")
+								.bearerFormat("JWT")))
+				.info(new Info().title("My API Service").version("1.0")
+						.description("Documentazione API con Spring Boot 3.5 e JDK 25"));
+	}
+}
